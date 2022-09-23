39da18fefc56fcdae5f76c7a9e7bddacf8cdc116
amit9021 <amit9021@gmail.com>
2022-09-23 11:52:54 +0300

    more performane improvment

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //jshint esversion:8
+
+let model;
+(async function loadChachModel() {
+  // load the model after page loaded and Chach it
+
+  const loadstart = window.performance.now();
+  model = await tf.loadGraphModel("tfjs_graph/model.json");
+  prediction = await model.predict(tf.zeros([1, 128, 128, 3]));
+  console.log("model loaded");
+  const loadends = window.performance.now();
+  $("#ui").show(600);
+  $("#loading").hide(600);
+  $("#loadTime").html(Math.round(loadends - loadstart) + ' ms');
+})();
+
 // listen to UPload a pic
 document
   .querySelector('input[type="file"]')
@@ -9,12 +24,15 @@ document
       var img = document.querySelector("img");
       img.onload = () => {
         //procsses the image
-        const dataImage = tf.browser.fromPixels(img).resizeBilinear([128, 128]);
+        const dataImage = tf.browser.fromPixels(img)
+          .resizeBilinear([128, 128]);
 
         const preprocessedInput = dataImage.expandDims();
 
         sendToPredict(preprocessedInput).then((result) =>
-          updateScore(result[0][0], result[0][1], window.performance.now() - timeStart)
+          updateScore(result[0][0],
+            result[0][1],
+            window.performance.now() - timeStart)
         );
         //predict(preprocessedInput);
         URL.revokeObjectURL(img.src); // no longer needed, free memory
@@ -47,24 +65,6 @@ function imgUrl() {
     URL.revokeObjectURL(img); // no longer needed, free memory
   };
 }
-
-let model;
-
-
-(async function loadChachModel() {
-  // load the model after page loaded
-  const loadstart = window.performance.now();
-  model = await tf.loadGraphModel("tfjs_graph/model.json");
-  prediction = await model.predict(tf.zeros([1, 128, 128, 3]));
-  console.log("model loaded");
-  const loadends = window.performance.now();
-  $("#ui").show(600);
-  $("#loading").hide(600);
-  $("#loadTime").html(Math.round(loadends - loadstart) + ' ms');
-})();
-
-
-
 
 async function sendToPredict(preprocessedInputToArray) {
   // const model = await tf.loadGraphModel("tfjs_graph/model.json");
